b92f9680a323db79ed1773aac95c7597dc3c2edd
Tejakoneti <tejarayal.koneti5@gmail.com>
2019-09-04 08:20:19 +0530

    All is well

--- src/main/java/com/test/testing/JavaProgramLearn.java
+++ src/main/java/com/test/testing/JavaProgramLearn.java
@@ -3,7 +3,7 @@ package com.test.testing;
 public class JavaProgramLearn {
 
 	public static void main(String[] args) {
-		System.out.println("fucking"+"git"+"eclipse"+"push");
+		System.out.println("all is well");
 	}
 
 }
